6a1dc8e579460db2e0ef314cb3fe36e0351c6b95
Khánh Duy <khanhduy0112110@gmail.com>
2025-11-11 18:34:42 +0700

    Revert "làm box tìm kiếm nâng cao , và sửa 1 số lỗi"
    This reverts commit a80692fd5ba24af21b64f69c3e9d524bf2cbb5e0.

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ inputSearch.addEventListener("keydown", function (event) {
     const keyword = inputSearch.value.trim();
 
     // Chuyển hướng bằng đường dẫn tuyệt đối (thay link này bằng link thật của bạn)
-    const absoluteURL = "products-find.html";
+    const absoluteURL = "products.html";
 
     // Nếu muốn gửi luôn từ khóa tìm kiếm:
     if (keyword !== "") {
